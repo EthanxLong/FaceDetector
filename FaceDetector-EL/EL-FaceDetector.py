a19fc954ca52bd0174a3837ca33d820d1f405d98
Ethan Long <55002363+EthanxLong@users.noreply.github.com>
2022-02-01 19:28:28 -0500

    Update EL-FaceDetector.py

--- FaceDetector-EL/EL-FaceDetector.py
+++ FaceDetector-EL/EL-FaceDetector.py
@@ -11,6 +11,7 @@ webcam = cv2.VideoCapture(0)
 r = randrange(256)
 g = randrange(256)
 b = randrange(256)
+
 # iterate over webcam frames
 while True:
     
@@ -37,20 +38,3 @@ while True:
 
 # Release webcam window     
 webcam.release()
-
-'''
-# Detect faces
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-
-# Draw rectangles around each face            vv Draw green here
-for (x, y, w, h) in face_coordinates:   
-    cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 10)
-
-#print(face_coordinates)
-
-# Display the image
-cv2.imshow("Ethan Long's Face Detector", img)
-cv2.waitKey()
-
-print("Code Completed")
-'''
